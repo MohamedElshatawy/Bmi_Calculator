741e54e8f49ac043993c2b7198d1b35da434b6f2
Mohamed Elshatawy <63617935+MohamedElshatawy@users.noreply.github.com>
2020-04-22 17:35:04 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
